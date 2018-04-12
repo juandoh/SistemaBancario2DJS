5be3ec3af12e7ca3498343516cbdf0c02e0d51ed
juandoh <juan.ordonez.huila@correounivalle.edu.co>
2018-04-11 23:14:50 -0500

    Add files via upload

--- src/Taller4/TarjetaCredito.java
+++ src/Taller4/TarjetaCredito.java
@@ -1,15 +1,17 @@
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Taller4;
+//package Taller4;
 
 /**
  *
  * @author arkai
  */
 public class TarjetaCredito {
+	
     public void retirar(){
         
     }
@@ -20,5 +22,9 @@ public class TarjetaCredito {
     
     public void pagar(){
         
+    }
+    
+    public void transferir( float importe, String cuentaDestino){
+    	
     }
 }
